238ef534c8600a2eaea3a52678cf75396e27751a
Travis Ray <tripp.machine@gmail.com>
2018-03-06 01:09:14 -0500

    Added more formatting for articles and streamer list

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,11 +6,17 @@
             <!-- Begin left column -->
             <div class="flex-1 mr-4">
               <div class="mb-3 items-center justify-center flex flex-col h-96 relative bg-cover" style="background-image: url('{{asset('images/test-post.jpg')}}');">
-                <span class="text-center font-title uppercase text-white text-4xl flex">Setup Streamlabs OBS</span>
+                <span class="text-center font-title uppercase text-white text-4xl mb-4 flex">Getting Started with Streamlabs OBS</span>
+                <span class="font-title uppercase">
+                  Posted in <a href="" class="text-white no-underline">STREAMING</a> on Mar, 6th 2018
+                </span>
               </div>
 
-              <div class="bg-test p-4 bg-cover">
-                This is a post.
+              <div class="bg-test items-center justify-center flex flex-col h-32 p-4 bg-cover">
+                  <span class="text-center font-title uppercase text-white mb-4 text-2xl flex">VERO OVERLAY</span>
+                  <span class="font-title uppercase">
+                    Posted in <a href="" class="text-white no-underline">TWITCH ASSETS</a> on Mar, 6th 2018
+                  </span>
               </div>
             </div>
 
@@ -18,8 +24,16 @@
             <div class="w-1/3 p-4">
               <span class="font-bold uppercase text-primary font-title mb-4 block">Streamers</span>
               @foreach($streamers as $s)
-                <div class="streamer mb-2">
-                  {{$s->name}}
+                <div class="streamer mb-2 bg-grey p-4">
+                  <div class="flex">
+                    <div class="flex-initial mr-4">
+                      <img src="http://placehold.it/42x42" class="rounded-full" alt="">
+                    </div>
+                    <div class="flex-1">
+                      <p>{{$s->name}}</p>
+                      offline
+                    </div>
+                  </div>
                 </div>
               @endforeach
             </div>
