0da388d7aa34739742c58ad845d3566336ce43de
Travis Ray <tripp.machine@gmail.com>
2018-03-11 04:56:50 -0400

    Tweaks to twitch integration

--- app/Http/Controllers/StreamerController.php
+++ app/Http/Controllers/StreamerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use romanzipp\Twitch\Twitch;
 use App\Streamer;
 
 class StreamerController extends Controller
@@ -18,21 +19,24 @@ class StreamerController extends Controller
         $this->middleware('auth')->except(['show']);
     }
 
-    public function show()
+    public function show(Twitch $twitch)
     {
-        $headers = [
-            'Authorization' => 'Bearer ' . env('TWITCH_CLIENT_ID'),
-            'Accept' => 'application/json'
-        ];
 
-        $client = new \GuzzleHttp\Client();
-        $streamer = Streamer::first();
-        $res = $client->request('GET', 'https://api.twitch.tv/helix/users', [
-            'headers' => $headers
-        ]);
-        dd($res);
+        // $streamers = Streamer::pluck('name')->toArray();
 
+        // Get User by Username
+        // $userResult = $twitch->getStreamsByUserNames($streamers);
+        // dd($userResult);
+        // $headers = [
+        //     'Authorization' => 'Bearer ' . env('TWITCH_CLIENT_ID'),
+        //     'Accept' => 'application/json'
+        // ];
 
+        // $client = new \GuzzleHttp\Client();
+        // $res = $client->request('GET', 'https://api.twitch.tv/helix/users', [
+        //     'headers' => $headers
+        // ]);
+        // dd($res);
     }
 
     public function create()
